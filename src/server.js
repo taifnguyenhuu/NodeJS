99add66443b150bd544b64caf7d9a5876ad8821a
Nguyen Huu Tai <nguyenhuutaidev@gmail.com>
2023-04-11 17:47:15 +0700

    Connecting to MySQL Database with Sequelize - Render Data Data View

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import bodyParser from "body-parser";
 import configviewEngine from "./config/viewEngine";
 import initWebRoutes from "./route/web";
+import connectDB from "./config/connectDB";
+
 require("dotenv").config();
 
 let app = express();
@@ -14,7 +16,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 configviewEngine(app);
 initWebRoutes(app);
 
-let port = process.env.PORT || 8080;
+connectDB();
+
+let port = process.env.PORT || 6969;
 //port === undefined => port = 8080
 
 app.listen(port, () => {
