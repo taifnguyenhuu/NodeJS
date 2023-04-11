af3f5276ffecb8057d0cf2393fe0abbe11e348e4
Nguyen Huu Tai <nguyenhuutaidev@gmail.com>
2023-04-11 10:28:23 +0700

    Applying MVC pattern

--- src/route/web.js
+++ src/route/web.js
@@ -1,12 +1,11 @@
 import express from "express";
+import homeController from "../controllers/homeController";
 
 let router = express.Router();
 
 let initWebRoutes = (app) => {
-  router.get("/", (req, res) => {
-    return res.send("Hello world with TaiHN");
-  });
-
+  router.get("/", homeController.getHomePage);
+  router.get("/About", homeController.getAboutPage);
   return app.use("/", router);
 };
 
