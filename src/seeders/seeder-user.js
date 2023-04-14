8c7d0dbed0648d226d303d02ec29d9b4b35951cc
Nguyen Huu Tai <nguyenhuutaidev@gmail.com>
2023-04-14 09:37:23 +0700

    Create Models, Migrations và Seeders

--- src/seeders/seeder-user.js
+++ src/seeders/seeder-user.js
@@ -1,12 +1,16 @@
 "use strict";
-
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert("Users", [
       {
-        firstName: "John",
-        lastName: "Doe",
-        email: "example@example.com",
+        email: "taihn@gmail.com",
+        password: "624680Tt$",
+        firstName: "Tai",
+        lastName: "Huu Nguyen",
+        address: "VietNam",
+        gender: 1,
+        typeRole: "ROLE",
+        keyRole: "R1",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
