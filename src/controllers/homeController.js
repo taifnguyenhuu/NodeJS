769443509509577e9993a9127f892c4992a89ded
Nguyen Huu Tai <nguyenhuutaidev@gmail.com>
2023-04-14 15:51:00 +0700

    Create new data into the database

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,4 +1,5 @@
 import db from "../models/index";
+import CRUDService from "../services/CRUDService";
 
 let getHomePage = async (req, res) => {
   try {
@@ -15,7 +16,18 @@ let getAboutPage = (req, res) => {
   return res.render("About/aboutPage.ejs");
 };
 
+let getCRUD = (req, res) => {
+  return res.render("crud.ejs");
+};
+
+let postCRUD = async (req, res) => {
+  let message = await CRUDService.createNewUser(req.body);
+  console.log(message);
+  return res.send("post crud from server");
+};
 module.exports = {
   getHomePage: getHomePage,
   getAboutPage: getAboutPage,
+  getCRUD: getCRUD,
+  postCRUD: postCRUD,
 };
--- src/route/web.js
+++ src/route/web.js
@@ -5,7 +5,10 @@ let router = express.Router();
 
 let initWebRoutes = (app) => {
   router.get("/", homeController.getHomePage);
-  router.get("/About", homeController.getAboutPage);
+  router.get("/about", homeController.getAboutPage);
+  router.get("/crud", homeController.getCRUD);
+
+  router.post("/post-crud", homeController.postCRUD);
   return app.use("/", router);
 };
 
