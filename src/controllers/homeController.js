c1f786bced401044b14127e9538133e1f9bff7a5
Nguyen Huu Tai <nguyenhuutaidev@gmail.com>
2023-04-15 21:45:59 +0700

    Update data from database

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -34,10 +34,31 @@ let displayGetCRUD = async (req, res) => {
     dataTable: data,
   });
 };
+let getEditCRUD = async (req, res) => {
+  let userId = req.query.id;
+  console.log(userId);
+  if (userId) {
+    let userData = await CRUDService.getUserInfoById(userId);
+    //check user data not found
+    //let userdata
+    return res.render("editCRUD.ejs", {
+      user: userData,
+    });
+  } else {
+    return res.send("User not found");
+  }
+};
+let putCRUD = async (req, res) => {
+  let data = req.body;
+  await CRUDService.updateUserData(data);
+  return res.send("done update");
+};
 module.exports = {
   getHomePage: getHomePage,
   getAboutPage: getAboutPage,
   getCRUD: getCRUD,
   postCRUD: postCRUD,
   displayGetCRUD: displayGetCRUD,
+  getEditCRUD: getEditCRUD,
+  putCRUD: putCRUD,
 };
