79d19fadc1648dae42566c6e6ef02609ec82ebc0
Nguyen Huu Tai <nguyenhuutaidev@gmail.com>
2023-04-23 08:45:52 +0700

    Import data from Excel and write API get allcode

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -59,11 +59,23 @@ let handleEditUser = async (req, res) => {
   let message = await userService.updateUserData(data);
   return res.status(200).json(message);
 };
-
+let getAllCode = async (req, res) => {
+  try {
+    let data = await userService.getAllCodeService(req.query.type);
+    return res.status(200).json(data);
+  } catch (e) {
+    console.log("Get all code error: ", e);
+    return res.status(200).json({
+      errCode: -1,
+      errMessage: "Err from server",
+    });
+  }
+};
 module.exports = {
   handleLogin: handleLogin,
   handleGetAllUsers: handleGetAllUsers,
   handleCreateNewUser: handleCreateNewUser,
   handleEditUser: handleEditUser,
   handleDeleteUser: handleDeleteUser,
+  getAllCode: getAllCode,
 };
