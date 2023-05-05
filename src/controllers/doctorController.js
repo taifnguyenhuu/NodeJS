299e4c4fcc250e8fb57148d37344113bd8b282de
Nguyen Huu Tai <nguyenhuutaidev@gmail.com>
2023-05-05 09:40:19 +0700

    Sequelize Relationship

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -39,8 +39,22 @@ let postInforDoctor = async (req, res) => {
     });
   }
 };
+
+let getDetailDoctorById = async (req, res) => {
+  try {
+    let infor = await doctorService.getDetailDoctorById(req.body.id);
+    return res.status(200).json(infor);
+  } catch (e) {
+    console.log(e);
+    return res.status(200).json({
+      errCode: -1,
+      errMessage: "Error from the server",
+    });
+  }
+};
 module.exports = {
   getTopDoctorHome: getTopDoctorHome,
   getAllDoctors: getAllDoctors,
   postInforDoctor: postInforDoctor,
+  getDetailDoctorById: getDetailDoctorById,
 };
