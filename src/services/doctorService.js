4a1bba0f540068658a3f9a3b9e582049861df127
Nguyen Huu Tai <nguyenhuutaidev@gmail.com>
2023-05-08 13:36:17 +0700

    Complete the section to view detailed doctor's information

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -1,3 +1,4 @@
+import { raw } from "body-parser";
 import db from "../models/index";
 let getTopDoctorHome = (limitInput) => {
   return new Promise(async (resolve, reject) => {
@@ -57,24 +58,39 @@ let saveDetailInforDoctor = (inputData) => {
       if (
         !inputData.doctorId ||
         !inputData.contentHTML ||
-        !inputData.contentMarkdown
+        !inputData.contentMarkdown ||
+        !inputData.action
       ) {
         resolve({
           errCode: 1,
           errMessage: "Missing parameter",
         });
       } else {
-        await db.Markdown.create({
-          contentHTML: inputData.contentHTML,
-          contentMarkdown: inputData.contentMarkdown,
-          description: inputData.description,
-          doctorId: inputData.doctorId,
+        if (inputData.action === "CREATE") {
+          await db.Markdown.create({
+            contentHTML: inputData.contentHTML,
+            contentMarkdown: inputData.contentMarkdown,
+            description: inputData.description,
+            doctorId: inputData.doctorId,
+          });
+        } else if (!inputData.action === "EDIT") {
+          let doctorMarkdown = await db.Markdown.findOne({
+            where: { doctorId: inputData.doctorId },
+            raw: false,
+          });
+          if (doctorMarkdown) {
+            doctorMarkdown.contentHTML = inputData.contentHTML;
+            doctorMarkdown.contentMarkdown = inputData.contentMarkdown;
+            doctorMarkdown.description = inputData.description;
+            doctorMarkdown.updataAt = new Date();
+            await doctorMarkdown.save();
+          }
+        }
+        resolve({
+          errCode: 0,
+          errMessage: "Save infor doctor succed",
         });
       }
-      resolve({
-        errCode: 0,
-        errMessage: "Save infor doctor succed",
-      });
     } catch (e) {
       reject(e);
     }
